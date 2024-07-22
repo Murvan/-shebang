98b52a1d18d16223f5ef71b0e12b6f1267f404b8
murvansky <gigamurvanidze96@gmail.com>
2024-07-22 15:45:16 +0400

    Looped Disk_Usage

--- disk_usage.sh
+++ disk_usage.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/bash
 
-threshold=90
-disk_usage=$(df -h / | awk 'NR==2 { print $6 }' | sed 's/%//')
+check_disk_space(){
+
+local threshold=90
+local disk_usage=$(df -h / | awk 'NR==2 { print $6 }' | sed 's/%//')
+
 
 if [ "${disk_usage}" -ge "$threshold" ]; then
    echo "Disk usage is ${disk_usage}, which is high risk!!!"
 else
    echo "Disk usage is ${disk_usage}, which is Normal"
 fi
+
+}
+
+
+while true
+do
+	check_disk_space
+	sleep 3600
+done
