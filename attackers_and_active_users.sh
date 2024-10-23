5a2232c72ce306162a88e02eae721aebe5c4467a
murvansky <gigamurvanidze96@gmail.com>
2024-10-23 11:29:35 +0400

    rar

--- attackers_and_active_users.sh
+++ attackers_and_active_users.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
 
-printf "\nList of Failed Connections:\n"
-
-tail -n 50  /var/log/auth.log | grep Failed | awk -F'invalid' '{print $2}'
+echo -e  "\nList of Failed Connections:"
+echo -e
+tail -n 50  /var/log/auth.log | grep Failed | awk -F'invalid' '{print $2}'| sed '/^$/d' 
+echo -e
 
 echo -e "------------------------------------------------------------------\n"
 echo -e  "List of active users:\n"
